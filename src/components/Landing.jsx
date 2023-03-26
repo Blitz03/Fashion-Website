b0c24cd8cb415affbc1878ef789852393bbfce3d
Mustafa <mrmrmustafag77@gmail.com>
2023-03-27 02:19:53 +0300

    Fixing Dependency Issue

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
 import { TfiAngleLeft, TfiAngleRight } from "react-icons/tfi";
