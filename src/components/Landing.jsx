6e90758987295718a051cc0024243dff2dba7bb2
Mustafa <mrmrmustafag77@gmail.com>
2023-03-27 02:18:13 +0300

    Fixing Dependency Issues

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useGlobalContext } from "../context";
 import { TfiAngleLeft, TfiAngleRight } from "react-icons/tfi";
@@ -17,13 +17,13 @@ export default function Landing() {
     );
   };
 
-  const goToNextSlide = () => {
+  const goToNextSlide = useCallback(() => {
     setCurrentSlide(
       currentSlide === landingSlides.length - 1 ? 0 : currentSlide + 1
     );
-  };
+  }, [currentSlide]);
 
-  useCallback(() => {
+  useEffect(() => {
     const timer = setInterval(() => {
       goToNextSlide();
     }, 4000);
